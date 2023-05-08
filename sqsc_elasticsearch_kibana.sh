c93bdd68f8644fd092291e09819c049d9d290d6a
Olivier Bourdon <olivier.bourdon@squarescale.com>
2023-05-08 10:38:33 +0200

    Add new demo project creating an self contained ElasticSearch + Kibana solution
    Please note that there is absolutely no resilience in the data as everything
    is handled by Docker containers in the current version.

---
+++ sqsc_elasticsearch_kibana.sh
@@ -0,0 +1,328 @@
+#!/usr/bin/env bash
+#
+# See http://docs.squarescale.com/CLI.html for more informations about sqsc CLI
+#
+# This script is used to deploy the SquareScale ElasticSearch + Kibana public project
+#
+# If a parameter is passed on the command line, it is taken as the name of the project
+# to be created
+#
+# It can also be customized via a set of environment variables
+#
+# SQSC_ENDPOINT: target SquareScale platform endpoint (defaults to empty aka https://www.squarescale.io)
+#
+# SQSC_TOKEN: security token generated via API Keys menu on platform endpoint
+# and used to run `sqsc login` successfully
+#
+# DRY_RUN: default to empty. Setting this to anything will show calls to be made by sqsc instead of
+#	running them
+#
+# VM_SIZE: (small, medium, large, dev, xsmall) Default is empty aka large
+# DISK_SIZE: Default is 60Gb
+#
+# RAM_SIZE: memory used by containers. Default is 4096
+# CPU_SIZE: CPU used by containers. Default is 1000Mhz
+#
+# Since version 1.4 of this script, multi-cloud providers support has been added
+# and therefore the following environment variables have been added
+#
+# CLOUD_PROVIDER: default to aws (can be switched to azure)
+# CLOUD_REGION: default to eu-west-1
+# CLOUD_CREDENTIALS: default to ""
+#
+# Support for multiple versions has also been added
+# DEFAULT_VERSION: default to 7.10.2
+#
+# Select multi node or single node deployment
+# INFRA_TYPE can be high-availabilityor single-node (default)
+
+SCRIPT_VERSION="1.0-2023-05-05"
+
+# Do not ask interactive user confirmation when creating resources
+NO_CONFIRM=${NO_CONFIRM:-"-yes"}
+
+echo -e "\nRunning $(basename "${BASH_SOURCE[0]}") version ${SCRIPT_VERSION}\n"
+
+# Exit on errors
+set -e
+
+# Set infra instances size (small, medium, large, dev, xsmall).
+# Default is medium because of RabbitMQ requirements
+#
+INFRA_NODE_SIZE=${VM_SIZE:-"large"}
+INFRA_NODE_DISK_SIZE=${DISK_SIZE:-"60"}
+INFRA_TYPE=${INFRA_TYPE:-"single-node"}
+
+# Set memory used by containers
+#
+RAM_SIZE=${RAM_SIZE:-"4096"}
+CPU_SIZE=${CPU_SIZE:-"1000"}
+
+# Default Postgres RDS version
+DEFAULT_VERSION=${DEFAULT_VERSION:-"7.10.2"}
+
+# Set project name according to 1st argument on command line or default
+# Convert to lower-case to avoid later errors
+# Remove non printable chars
+PROJECT_NAME=$(echo "${1:-"sqsc-elasticsearch-kibana"}" | tr '[:upper:]' '[:lower:]' | tr -dc '[:print:]')
+
+if [ -z "${PROJECT_NAME}" ]; then
+	echo "${1:-"sqsc-elasticsearch-kibana"} is not a valid project name (non-printable characters)"
+	exit 1
+fi
+
+FULL_PROJECT_NAME="${PROJECT_NAME}"
+if [ -n "${ORGANIZATION}" ]; then
+	FULL_PROJECT_NAME="${ORGANIZATION}/${PROJECT_NAME}"
+fi
+
+# Look up for sqsc CLI binary in PATH
+SQSC_BIN=$(command -v sqsc)
+SQSC_VERSION=$(${SQSC_BIN} version | awk '{print $3}')
+REQUIRED_SQSC_VERSION="1.1.4"
+if [ "${SQSC_VERSION}" != "${REQUIRED_SQSC_VERSION}" ]; then
+	echo "sqsc CLI version ${REQUIRED_SQSC_VERSION} required (${SQSC_VERSION} detected)"
+	exit 1
+fi
+
+# Show calls to sqsc instead of executing them
+if [ -n "${DRY_RUN}" ]; then
+	SQSC_BIN="echo ${SQSC_BIN}"
+fi
+
+if [ -z "${SQSC_TOKEN}" ]; then
+	echo "You need to set SQSC_TOKEN to an existing and active API key in your account profile"
+	exit 1
+fi
+
+# Set default endpoint (none => production aka https://www.squarescale.io)
+if [ -z "${SQSC_ENDPOINT}" ]; then
+	export SQSC_ENDPOINT="https://www.squarescale.io"
+	echo "Using default for SQSC_ENDPOINT: ${SQSC_ENDPOINT}"
+fi
+
+# Check current SquareScale endpoint status
+${SQSC_BIN} status || ${SQSC_BIN} login
+
+# Cloud related environment variables
+CLOUD_PROVIDER=${CLOUD_PROVIDER:-"aws"}
+CLOUD_REGION=${CLOUD_REGION:-"eu-west-1"}
+CLOUD_CREDENTIALS=${CLOUD_CREDENTIALS:-""}
+
+if [[ -z "${CLOUD_PROVIDER}" || ( "${CLOUD_PROVIDER}" != "aws" && "${CLOUD_PROVIDER}" != "azure"  && "${CLOUD_PROVIDER}" != "outscale" ) ]]; then
+	echo "CLOUD_PROVIDER=${CLOUD_PROVIDER} unsupported (only aws/azure/outscale)"
+	exit 1
+fi
+
+# Function which wait for project cluster to
+# be able to schedule containers
+#
+function wait_for_project_scheduling() {
+	echo "Waiting for project to be able to schedule containers"
+	if [ -n "${DRY_RUN}" ]; then
+		return
+	fi
+	while true; do
+		# shellcheck disable=SC2207
+		eval "$(${SQSC_BIN} project get -project-name "${FULL_PROJECT_NAME}" | grep -Ev '^Slack|^Age' | awk 'NF>1{print}' | sed -e 's/: /="/' -e 's/$/"/')"
+		# shellcheck disable=SC2154
+		if [ "${Status}" == "error" ]; then
+			echo "${PROJECT_NAME} provisionning has encountered an error"
+			exit 1
+		fi
+		# shellcheck disable=SC2207,SC2001
+		available=($(echo "${Nodes}" | sed -e 's?/? ?'))
+		if [ "${Status}" != "ok" ] || [ "${available[0]}" == "0" ] || [ -z "${available[0]}" ]; then
+			echo "Project ${PROJECT_NAME} is not ready to schedule any containers yet"
+			sleep 5
+		else
+			break
+		fi
+	done
+}
+
+# Function which creates a service
+#
+# Parameters:
+# 1) Docker Hub container image name
+# 2) Memory size required (optional)
+# 3) CPU size required (optional)
+#
+function add_service() {
+	wait_for_project_scheduling
+	container_image=$(echo "$1" | awk -F/ '{print $NF}' | awk -F: '{print $1}')
+	cur_containers=$(show_containers)
+	if echo "$cur_containers" | grep -Eq "^${container_image}\s\s*"; then
+		echo "${PROJECT_NAME} already configured with service container $container_image. Skipping..."
+	else
+		echo "Adding container service $container_image"
+		${SQSC_BIN} service add -project-uuid "${PROJECT_UUID}" -image "$1"
+	fi
+	if [ -n "$2" ]; then
+		cur_val=$(${SQSC_BIN} service show -project-uuid "${PROJECT_UUID}" -name "$container_image" | grep ^Mem | awk '{print $(NF-1)}')
+		if [ "$cur_val" != "$2" ]; then
+			echo "Increasing $1 container memory to $2"
+			${SQSC_BIN} service set -project-uuid "${PROJECT_UUID}" -service "$container_image" -memory "$2"
+		else
+			echo "$1 container memory already set to $2"
+		fi
+	fi
+	if [ -n "$3" ]; then
+		cur_val=$(${SQSC_BIN} service show -project-uuid "${PROJECT_UUID}" -name "$container_image" | grep ^CPU | awk '{print $(NF-1)}')
+		if [ "$cur_val" != "$3" ]; then
+			echo "Increasing $1 container CPU to $3"
+			${SQSC_BIN} service set -project-uuid "${PROJECT_UUID}" -service "$container_image" -cpu "$3"
+		else
+			echo "$1 container CPU already set to $3"
+		fi
+	fi
+}
+
+# Function which checks if environment variable already set
+# if not, then set it to given value (2nd parameter)
+function set_env_var(){
+	# sqsc env get returns error if variable is not set already
+	# and -e has been activated globally at the top of this script
+	set +e
+	v=$(${SQSC_BIN} env get -project-uuid "${PROJECT_UUID}" "$1" 2>/dev/null)
+	# Error: aka variable not defined
+	# shellcheck disable=SC2181
+	if [ $? -eq 0 ] && [ "$v" == "$2" ]; then
+		echo "$1 already set to $2. Skipping..."
+	else
+		${SQSC_BIN} env set -project-uuid "${PROJECT_UUID}" "$1" "$2"
+	fi
+	# Restore script failure on further errors
+	set -e
+}
+
+# Function creating the project
+# this is the main entry point
+function create_project(){
+	projects=$(${SQSC_BIN} project list)
+	# take organization into account for proper retrieval (creation OK)
+	# in case project with same name but no org or not same org
+	search_pattern="^${PROJECT_NAME}\s\s*.*\s\s*${ORGANIZATION}\s\s*"
+	if echo "$projects" | grep -Eq "${search_pattern}"; then
+		echo "${PROJECT_NAME} already created. Skipping..."
+		if echo "$projects" | grep -Eq "${search_pattern}no_infra\s\s*"; then
+			echo "${PROJECT_NAME} starting provisionning..."
+			${SQSC_BIN} project provision "${ORG_OPTIONS}" -project-name "${FULL_PROJECT_NAME}"
+		elif echo "$projects" | grep -Eq "${search_pattern}error\s\s*"; then
+			echo "${PROJECT_NAME} provisionning has encountered an error"
+			exit 1
+		else
+			echo "${PROJECT_NAME} already provisionning. Skipping..."
+		fi
+	else
+		if [ -n "${ORGANIZATION}" ]; then
+			ORG_OPTIONS="-organization ${ORGANIZATION}"
+		fi
+		if [ -n "${SLACK_WEB_HOOK}" ]; then
+			SLACK_OPTIONS="-slackbot ${SLACK_WEB_HOOK}"
+		fi
+		if [ -z "${CLOUD_CREDENTIALS}" ]; then
+			echo "You need to set CLOUD_CREDENTIALS to an existing IaaS credential in your account profile"
+			exit 1
+		fi
+		eval "${SQSC_BIN} project create ${ORG_OPTIONS} ${SLACK_OPTIONS} ${NO_CONFIRM} -provider \"${CLOUD_PROVIDER}\" -region \"${CLOUD_REGION}\" -credential \"${CLOUD_CREDENTIALS}\" -infra-type \"${INFRA_TYPE}\" -root-disk-size \"${INFRA_NODE_DISK_SIZE}\" -node-size \"${INFRA_NODE_SIZE}\" -name \"${PROJECT_NAME}\""
+		projects=$(${SQSC_BIN} project list)
+	fi
+	PROJECT_UUID=$(echo "$projects" | grep -E "${search_pattern}" | awk '{print $2}')
+
+	# To send notifications on #demoapp SquareScale Slack channel
+	if [ -n "${SLACK_WEB_HOOK}" ]; then
+		slackwebhook="$(${SQSC_BIN} project slackbot -project-uuid "${PROJECT_UUID}")"
+		tobeupdated=true
+		if [ -n "$slackwebhook" ]; then
+			if [ "$slackwebhook" == "${SLACK_WEB_HOOK}" ]; then
+				echo "${PROJECT_NAME} already configured with Slack. Skipping..."
+				tobeupdated=false
+			fi
+		fi
+		if $tobeupdated; then
+			${SQSC_BIN} project slackbot -project-uuid "${PROJECT_UUID}" "${SLACK_WEB_HOOK}"
+		fi
+	else
+		echo "SLACK_WEB_HOOK not set: ignoring ..."
+	fi
+}
+
+function show_containers(){
+	${SQSC_BIN} service list -project-uuid "${PROJECT_UUID}"
+}
+
+function wait_containers(){
+	echo "Waiting for containers to be running"
+	if [ -n "${DRY_RUN}" ]; then
+		return
+	fi
+	while true; do
+		c=$(show_containers | while read -r -a container; do
+			if [ "${container[0]}" != "Name" ]; then
+				r=$(echo "${container[1]}" | awk -F/ '$1==$2{print 1}')
+				if [ -z "${r}" ]; then
+					echo "${container[0]}"
+					break
+				fi
+			fi
+		done)
+		if [ -n "${c}" ]; then
+			echo "${c} not ready"
+			sleep 5
+		else
+			echo -e 'All containers ready\n'
+			break
+		fi
+	done
+}
+
+function add_services(){
+	add_service "${ELASTICSEARCH_DOCKER_IMAGE:-docker.elastic.co/elasticsearch/elasticsearch}:${DEFAULT_VERSION}" "${RAM_SIZE}" "${CPU_SIZE}"
+	add_service "${KIBANA_DOCKER_IMAGE:-docker.elastic.co/kibana/kibana}:${DEFAULT_VERSION}" "${RAM_SIZE}" "${CPU_SIZE}"
+}
+
+function configure_services_environment(){
+	# TODO: cleanup tmp files on error
+	# TODO: check existing config before overriding and rescheduling
+	echo -e '{\n"node.name": "es01",\n"cluster.name": "es-docker-cluster",\n"bootstrap.memory_lock": "false",\n"discovery.type": "single-node"}\n' | jq -rS . >/tmp/es-svc.json
+	${SQSC_BIN} service set -project-uuid "${PROJECT_UUID}" -service elasticsearch -instances 1 -env /tmp/es-svc.json
+	rm -f /tmp/es-svc.json
+	echo -e '{\n"ELASTICSEARCH_HOSTS": "http://elasticsearch.service.consul:9200",\n"SERVER_BASEPATH": "/kibana",\n"SERVER_REWRITEBASEPATH": "true"}\n' | jq -rS . >/tmp/ki-svc.json
+	${SQSC_BIN} service set -project-uuid "${PROJECT_UUID}" -service kibana -instances 1 -env /tmp/ki-svc.json
+	rm -f /tmp/ki-svc.json
+}
+
+function set_network_rules(){
+	net_rule=$(${SQSC_BIN} network-rule list -project-uuid "${PROJECT_UUID}" -service-name elasticsearch)
+	if echo "$net_rule" | grep -Eq '^es\s*http/9200\s*http/80\s*/$'; then
+		echo "Network rule already configured for elasticsearch. Skipping..."
+	else
+		# TODO: see if this needs to be parametrized (duplicate/resource already exist)
+		echo "Adding network rule for elasticsearch"
+		${SQSC_BIN} network-rule create -project-uuid "${PROJECT_UUID}" -name "es" -internal-protocol "http" -internal-port 9200 -external-protocol "http" -service-name "elasticsearch" -path-prefix "/"
+	fi
+	net_rule=$(${SQSC_BIN} network-rule list -project-uuid "${PROJECT_UUID}" -service-name kibana)
+	if echo "$net_rule" | grep -Eq '^ki\s*http/5601\s*http/80\s*/kibana$'; then
+		echo "Network rule already configured for kibana. Skipping..."
+	else
+		# TODO: see if this needs to be parametrized (duplicate/resource already exist)
+		echo "Adding network rule for kibana"
+		${SQSC_BIN} network-rule create -project-uuid "${PROJECT_UUID}" -name "ki" -internal-protocol "http" -internal-port 5601 -external-protocol "http" -service-name "kibana" -path-prefix "/kibana"
+	fi
+}
+
+function show_url(){
+	echo -e 'Load balancer informations\n'
+	lb_url=$(${SQSC_BIN} lb list -project-uuid "${PROJECT_UUID}" | grep '://' | awk '{print $NF}')
+	echo -e "ElasticSearch URL:\t${lb_url}\nKibana URL:\t\t${lb_url}/kibana\n"
+}
+
+create_project
+add_services
+configure_services_environment
+set_network_rules
+
+# Show all
+wait_containers
+show_url
