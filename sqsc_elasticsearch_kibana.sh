7aba1d18c3a96f73ac09c9bff2667597fb625b38
Olivier Bourdon <olivier.bourdon@squarescale.com>
2023-08-17 11:35:27 +0200

    Update required SquareScale CLI version

--- sqsc_elasticsearch_kibana.sh
+++ sqsc_elasticsearch_kibana.sh
@@ -79,7 +79,7 @@ fi
 # Look up for sqsc CLI binary in PATH
 SQSC_BIN=$(command -v sqsc)
 SQSC_VERSION=$(${SQSC_BIN} version | awk '{print $3}')
-REQUIRED_SQSC_VERSION="1.1.4"
+REQUIRED_SQSC_VERSION="1.1.5"
 if [ "${SQSC_VERSION}" != "${REQUIRED_SQSC_VERSION}" ]; then
 	echo "sqsc CLI version ${REQUIRED_SQSC_VERSION} required (${SQSC_VERSION} detected)"
 	exit 1
